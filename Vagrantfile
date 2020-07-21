42ec3ec4012311ab16a4c550c9e23f82b5d5a6ff
work@jacobthomas.net <work@jacobthomas.net>
2020-07-21 22:48:22 +0000

    Second set of changes

--- Vagrantfile
+++ Vagrantfile
@@ -8,7 +8,7 @@ opts = GetoptLong.new(
     ["--nodes", GetoptLong::OPTIONAL_ARGUMENT]
 )
 
-numberOfNodes=2 
+numberOfNodes=3
 
 opts.ordering=(GetoptLong::REQUIRE_ORDER)
 
@@ -16,10 +16,13 @@ opts.each do |opt, arg|
   case opt
     when '--nodes'
       numberOfNodes= Integer(arg)
-
   end
+  print "\n"
   print "Number of Nodes , #{numberOfNodes}"
+  print "\n"
 end
+
+
 
 ######## Begin main vagrant file
 
@@ -28,9 +31,6 @@ BOX_IMAGE = "oraclelinux/7"
 BOX_URL = "https://oracle.github.io/vagrant-projects/boxes/oraclelinux/7.json"
 NODE_COUNT=numberOfNodes 
 var_user_name ="Jacob"
-acceptOTNLicenseAgreement="Y"
-otnusername="jacobt_412@outlook.com"
-otnpassword="Gumby1234"
 shiphomeurl="http://download.oracle.com/otn/nt/middleware/12c/12213/fmw_12.2.1.3.0_wls_Disk1_1of1.zip"
 jdkurl="https://download.oracle.com/otn/java/jdk/8u131-b11/d54c1d3a095b4ff2b6607d096fa80163/jdk-8u131-linux-x64.tar.gz"
 wlsversion="12.2.1.4.0"
@@ -39,70 +39,89 @@ jdkversion="jdk1.8.0_251"
 
 Vagrant.configure("2") do |config|
 
-  if Vagrant.has_plugin?("vagrant-proxyconf")
-    puts "getting Proxy Configuration from Host..."
-    if ENV["http_proxy"]
-      puts "http_proxy: " + ENV["http_proxy"]
-      config.proxy.http     = ENV["http_proxy"]
-    end
-    if ENV["https_proxy"]
-      puts "https_proxy: " + ENV["https_proxy"]
-      config.proxy.https    = ENV["https_proxy"]
-    end
-    if ENV["no_proxy"]
-      config.proxy.no_proxy = ENV["no_proxy"]
-    end
-  end
 
-  config.vm.define "admin" do |subconfig|
+
+
+if Vagrant.has_plugin?("vagrant-proxyconf")
+    puts "getting Proxy Configuration from Host..."
+if ENV["http_proxy"]
+    puts "http_proxy: " + ENV["http_proxy"]
+    config.proxy.http     = ENV["http_proxy"]
+end
+if ENV["https_proxy"]
+    puts "https_proxy: " + ENV["https_proxy"]
+    config.proxy.https    = ENV["https_proxy"]
+end
+if ENV["no_proxy"]
+    no_proxy_list.concat(ENV["no_proxy"])
+    no_proxy_list.concat(",")
+    no_proxy_list.concat("10.0.0.10")
+    (1..NODE_COUNT).each do |i|
+        no_proxy_list.concat(",")
+        no_proxy_list.concat("10.0.0.#{i + 10}")
+    end
+    puts "no_proxy: " + no_proxy_list
+    config.proxy.no_proxy = no_proxy_list 
+end
+end
+
+config.vm.define "admin" do |subconfig|
+subconfig.vm.box = BOX_IMAGE
+subconfig.vm.box_url = BOX_URL
+#subconfig.name ="admin"
+subconfig.vm.hostname = "admin"
+subconfig.vm.network :private_network, ip: "10.0.0.10"
+subconfig.vm.provision "shell", inline: <<-SHELL
+    echo "H E L L O     W O R L D ============> ADMIN NODE"    
+    SHELL
+subconfig.vm.provision "shell", path: "scripts/createadmin.sh", env: {
+"ADMINURL"  => "10.0.0.10"
+} 
+
+end
+  
+(1..NODE_COUNT).each do |i|
+config.vm.define "managed#{i}" do |subconfig|
     subconfig.vm.box = BOX_IMAGE
     subconfig.vm.box_url = BOX_URL
-    #subconfig.name ="admin"
-    subconfig.vm.hostname = "admin"
-    subconfig.vm.network :private_network, ip: "10.0.0.10"
-  end
-  
-  (1..NODE_COUNT).each do |i|
-    config.vm.define "managed#{i}" do |subconfig|
-      subconfig.vm.box = BOX_IMAGE
-      subconfig.vm.box_url = BOX_URL
 
-      subconfig.vm.hostname = "managed#{i}"
-      subconfig.vm.network :private_network, ip: "10.0.0.#{i + 10}"
-    end
-  end
+    subconfig.vm.hostname = "managed#{i}"
+    subconfig.vm.network :private_network, ip: "10.0.0.#{i + 10}"
+    subconfig.vm.provision "shell", inline: <<-SHELL
+    echo "H E L L O     W O R L D ==========> MANAGED NODE"
+    echo "H E L L O     W O R L D  #{i}" 
+    SHELL
+end
+end
 
-  config.vm.provision "shell", inline: <<-SHELL
-     echo "Hello World"
-     cat >> /etc/motd << EOF
-	*******************************************************
-	**                 Hello there,                      **
-	**      Welcome to a VM that was customized with     **
-	**            the vagrant shell provisioner          **
-	*******************************************************
+config.vm.provision "shell", inline: <<-SHELL
+    echo "Hello World"
+    cat >> /etc/motd << EOF
+*******************************************************
+**                 Hello there,                      **
+**      Welcome to a VM that was customized with     **
+**            the vagrant shell provisioner          **
+*******************************************************
 EOF
-  SHELL
+SHELL
 
-   config.vm.provision "shell", inline: <<-SHELL
-     echo "Installing zip unzip wget rng-tools"
-     yum install -y zip unzip wget rng-tools
+config.vm.provision "shell", inline: <<-SHELL
+    echo "Installing zip unzip wget rng-tools"
+    yum install -y zip unzip wget rng-tools
 
-     echo "Setting up rngd utils as a service"
-     systemctl enable rngd 
-     systemctl status rngd
-     systemctl start rngd
-     systemctl status rngd
+    echo "Setting up rngd utils as a service"
+    systemctl enable rngd 
+    systemctl status rngd
+    systemctl start rngd
+    systemctl status rngd
 
-   SHELL
+SHELL
 
 # Enable provisioning with a shell script
 
 #   config.vm.provision :shell, inline: "echo 'source /vagrant/scripts/setenv.sh' > /etc/profile.d/sa-environment.sh", :run => 'always'
    config.vm.provision "shell", path: "scripts/setup.sh", env: {
          "NAME"         => var_user_name,
-	 "OTNLICENSE"   => acceptOTNLicenseAgreement,
-         "USEROTN"      => otnusername,
-	 "PASSWORD"     => otnpassword,
          "WLSURL"       => shiphomeurl,
 	 "JAVAURL"      => jdkurl,
 	 "VERSIONWLS"   => wlsversion,
@@ -110,3 +129,4 @@ EOF
    }
 
 end
+
---
+++ scripts/createadmin.sh
@@ -0,0 +1,212 @@
+#!/bin/bash
+
+set -e
+
+
+#Creates weblogic deployment model for cluster domain admin setup
+function create_admin_model()
+{
+    echo "Creating admin domain model"
+    cat <<EOF >$DOMAIN_PATH/admin-domain.yaml
+domainInfo:
+   AdminUserName: "$wlsUserName"
+   AdminPassword: "$wlsPassword"
+   ServerStartMode: prod
+topology:
+   Name: "$wlsDomainName"
+   AdminServerName: admin
+   Machine:
+     '$nmHost':
+         NodeManager:
+             ListenAddress: "$nmHost"
+             ListenPort: $nmPort
+             NMType : ssl
+   Cluster:
+        '$wlsClusterName':
+             MigrationBasis: 'consensus'
+   Server:
+        '$wlsServerName': 
+            ListenPort: $wlsAdminPort
+            RestartDelaySeconds: 10
+            SSL:
+               ListenPort: $wlsSSLAdminPort
+               Enabled: true
+   SecurityConfiguration:
+       NodeManagerUsername: "$wlsUserName"
+       NodeManagerPasswordEncrypted: "$wlsPassword"
+EOF
+}
+
+function create_adminSetup()
+{
+	mkdir -p $DOMAIN_PATH
+
+	echo "created doamin directory $DOMAIN_PATH"
+
+	unzip -o $BASE_DIR/weblogic-deploy.zip -d $DOMAIN_PATH
+	create_admin_model
+	chown -R $username:$groupname $DOMAIN_PATH
+	runuser -l oracle -c ". $oracleHome/oracle_common/common/bin/setWlstEnv.sh; $DOMAIN_PATH/weblogic-deploy/bin/createDomain.sh -oracle_home $oracleHome -domain_parent $DOMAIN_PATH  -domain_type WLS -model_file $DOMAIN_PATH/admin-domain.yaml"
+
+	echo "Created admin domain ......."
+
+}
+
+#Function to setup admin boot properties
+function admin_boot_setup()
+{
+ echo "Creating admin boot properties"
+ #Create the boot.properties directory
+ mkdir -p "$DOMAIN_PATH/$wlsDomainName/servers/admin/security"
+ echo "username=$wlsUserName" > "$DOMAIN_PATH/$wlsDomainName/servers/admin/security/boot.properties"
+ echo "password=$wlsPassword" >> "$DOMAIN_PATH/$wlsDomainName/servers/admin/security/boot.properties"
+ chown -R $username:$groupname $DOMAIN_PATH/$wlsDomainName/servers
+}
+
+# Create systemctl service for nodemanager
+function create_nodemanager_service()
+{
+ echo "Setting CrashRecoveryEnabled true at $DOMAIN_PATH/$wlsDomainName/nodemanager/nodemanager.properties"
+ sed -i.bak -e 's/CrashRecoveryEnabled=false/CrashRecoveryEnabled=true/g'  $DOMAIN_PATH/$wlsDomainName/nodemanager/nodemanager.properties
+ if [ $? != 0 ];
+ then
+   echo "Warning : Failed in setting option CrashRecoveryEnabled=true. Continuing without the option."
+   mv $DOMAIN_PATH/nodemanager/nodemanager.properties.bak $DOMAIN_PATH/$wlsDomainName/nodemanager/nodemanager.properties
+ fi
+ sudo chown -R $username:$groupname $DOMAIN_PATH/$wlsDomainName/nodemanager/nodemanager.properties*
+ echo "Creating NodeManager service"
+ cat <<EOF >/etc/systemd/system/wls_nodemanager.service
+ [Unit]
+Description=WebLogic nodemanager service
+
+[Service]
+Type=simple
+# Note that the following three parameters should be changed to the correct paths
+# on your own system
+WorkingDirectory="$DOMAIN_PATH/$wlsDomainName"
+ExecStart="$DOMAIN_PATH/$wlsDomainName/bin/startNodeManager.sh"
+ExecStop="$DOMAIN_PATH/$wlsDomainName/bin/stopNodeManager.sh"
+User=oracle
+Group=oracle
+KillMode=process
+LimitNOFILE=65535
+
+[Install]
+WantedBy=multi-user.target
+EOF
+}
+
+# This function to create adminserver service
+function create_adminserver_service()
+{
+ echo "Creating admin server service"
+ cat <<EOF >/etc/systemd/system/wls_admin.service
+[Unit]
+Description=WebLogic Adminserver service
+
+[Service]
+Type=simple
+WorkingDirectory="$DOMAIN_PATH/$wlsDomainName"
+ExecStart="$DOMAIN_PATH/$wlsDomainName/startWebLogic.sh"
+ExecStop="$DOMAIN_PATH/$wlsDomainName/bin/stopWebLogic.sh"
+User=oracle
+Group=oracle
+KillMode=process
+LimitNOFILE=65535
+
+[Install]
+WantedBy=multi-user.target
+EOF
+}
+
+function enabledAndStartNodeManagerService()
+{
+  systemctl enable wls_nodemanager
+  systemctl daemon-reload
+
+  attempt=1
+  while [[ $attempt -lt 6 ]]
+  do
+     echo "Starting nodemanager service attempt $attempt"
+     systemctl start wls_nodemanager
+     sleep 1m
+     attempt=`expr $attempt + 1`
+     systemctl status wls_nodemanager | grep running
+     if [[ $? == 0 ]];
+     then
+         echo "wls_nodemanager service started successfully"
+	 break
+     fi
+     sleep 3m
+ done
+}
+
+function enableAndStartAdminServerService()
+{
+  systemctl enable wls_admin
+  systemctl daemon-reload
+  echo "Starting admin server service"
+  systemctl start wls_admin
+
+}
+
+#This function to wait for admin server
+function wait_for_admin()
+{
+ #wait for admin to start
+count=1
+#export CHECK_URL="http://$wlsAdminURL/weblogic/ready"
+export CHECK_URL="http://127.0.0.1:7001/weblogic/ready"
+echo "Ready App url ::::::: $CHECK_URL "
+status=`curl --insecure -ILs $CHECK_URL | tac | grep -m1 HTTP/1.1 | awk {'print $2'}`
+echo "Waiting for admin server to start"
+while [[ "$status" != "200" ]]
+do
+  echo $status
+  echo "."
+   
+  count=$((count+1))
+  if [ $count -le 30 ];
+  then
+      sleep 1m
+  else
+     echo "Error : Maximum attempts exceeded while starting admin server"
+     exit 1
+  fi
+  status=`curl --insecure -ILs $CHECK_URL | tac | grep -m1 HTTP/1.1 | awk {'print $2'}`
+  if [ "$status" == "200" ];
+  then
+     echo "Server $wlsServerName started succesfully..."
+     break
+  fi
+done
+}
+
+echo "Creating Admin Setup ......................."
+
+DOMAIN_PATH="/u01/domains"
+BASE_DIR="/vagrant/installers"
+username="oracle"
+groupname="oracle"
+wlsDomainName="clusterDomain"
+wlsUserName="system"
+wlsPassword="gumby1234"
+wlsServerName="adminserver"
+wlsAdminHost=$ADMINURL
+oracleHome="/u01/app/wls/install/oracle/middleware/oracle_home"
+wlsAdminPort=7001
+wlsSSLAdminPort=7002
+wlsManagedPort=8001
+wlsAdminURL="$wlsAdminHost:$wlsAdminPort"
+wlsClusterName="cluster1"
+nmHost=`hostname`
+nmPort=5556
+
+
+create_adminSetup
+create_nodemanager_service
+admin_boot_setup
+create_adminserver_service
+enabledAndStartNodeManagerService
+enableAndStartAdminServerService
+wait_for_admin
